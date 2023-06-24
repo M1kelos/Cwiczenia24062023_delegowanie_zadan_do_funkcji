b16af3d5bddca5297bf57cd82af8c16d8d2aa7a8
M1kelos <128227021+M1kelos@users.noreply.github.com>
2023-06-24 20:30:15 +0200

    Add files via upload

---
+++ 06_Rekurencyjna_srednia_arytmetyczna.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] numbers = { 2.5, 3.8, 1.2, 4.6, 2.1 };
+        int size = numbers.Length;
+
+        double average = CalculateRecursiveAverage(numbers, size);
+        Console.WriteLine($"Średnia arytmetyczna: {average}");
+    }
+
+    static double CalculateRecursiveAverage(double[] numbers, int size)
+    {
+        if (size == 0)
+        {
+            throw new ArgumentException("Tablica jest pusta.");
+        }
+
+        return CalculateSum(numbers, size) / size;
+    }
+
+    static double CalculateSum(double[] numbers, int size)
+    {
+        if (size == 0)
+        {
+            return 0.0;
+        }
+
+        return numbers[size - 1] + CalculateSum(numbers, size - 1);
+    }
+}
